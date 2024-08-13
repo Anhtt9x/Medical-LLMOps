3e289ee0c3458ef38b3f1e4e7b05a9245e5cf5be
Anhtt9x <anhtt454598@gmail.com>
2024-08-13 15:52:05 +0700

    update_docker

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup ,find_packages
+
+
+setup(name="Medical-LLMOps",
+      version="0.0.0",
+      author="Anhtt9x",
+      author_email="anhtt454598@gmail.com",
+      packages=find_packages(where=[".","src"]))
